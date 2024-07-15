407f928a6f00c7acac00eb8ecd24364b6efb519d
MainLx <158014098+MainLx@users.noreply.github.com>
2024-07-15 16:52:47 +0200

    Add files via upload

---
+++ #Part 6 Primer proyecto.py
@@ -0,0 +1,9 @@
+# Primer proyecto
+#1. Crea una bienvenida para tu programa.
+print("bienvenidos al himalaya este es un generador de nombres para bandas.")
+#2. Preguntale al usuario en que ciudad crecio.
+city = input("en que ciudad creciste durante tu infancia?.")
+#3. Preguntale por el nombre de su mascota.
+pet = input("cual es el nombre de tu mascota?.")
+#4. Combina el nombre de su ciudad natal con el nombre de su mascota y muestrales el nombre de su banda
+print("el nombre de tu mascosta podria ser:" + city + " " + pet)
